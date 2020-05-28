0de71f0a1fff83c8759ae2f19993f5b1b5621bd0
Gordon Wallace <gordwilling@yahoo.ca>
2020-05-27 20:25:22 -0400

    moved resume data directory to a sensible place

--- web-templates/resume/src/resume.js
+++ web-templates/resume/src/resume.js
@@ -3,10 +3,10 @@ import {downloadTemplateData} from "../../lib/download.js";
 
 function initTemplates() {
     const dataLocations = {
-        basics: "../data/basics.json",
-        experience: "../data/experience.json",
-        education: "../data/education.json",
-        technology: "../data/technology.json"
+        basics: "./data/basics.json",
+        experience: "./data/experience.json",
+        education: "./data/education.json",
+        technology: "./data/technology.json"
     }
 
     const templateRefs = Array.from(document.querySelectorAll("[data-template]"))
